58946676ba237eaef4aa91b1a0df8c6f008c6177
sDuyToans <duytoannguyenledh4@gmail.com>
2022-05-15 22:12:31 +0700

    con responsive ti nua

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
- // function to set a given theme/color-scheme
- function setTheme(themeName) {
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
@@ -20,17 +20,55 @@ function toggleTheme() {
         document.getElementById('slider').checked = false;
     } else {
         setTheme('theme-light');
-      document.getElementById('slider').checked = true;
+        document.getElementById('slider').checked = true;
     }
 })();
 
-$(function() {
-    $(window).on("scroll", function() {
-        if($(window).scrollTop() > 50) {
+$(function () {
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 50) {
             $(".header").addClass("active");
         } else {
             //remove the background property so it comes transparent again (defined in your css)
-           $(".header").removeClass("active");
+            $(".header").removeClass("active");
         }
     });
 });
+
+$(document).ready(function () {
+    $("#lightgallery").lightGallery();
+});
+
+$(document).ready(function() {
+  
+    //Pour afficher le bouton 
+      var offset = 120;
+      var duration = 500;
+      $(window).scroll(function() {
+          if (jQuery(this).scrollTop() > offset) {
+              $('.back-to-top').fadeIn(duration);
+          } else {
+              $('.back-to-top').fadeOut(duration);
+          }
+      });
+    
+    //Pour le smoth scroll
+    $(function() {
+       $('a[href*=#]:not([href=#])').click(function() {
+           if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+         if (target.length) {
+                 $('html,body').animate({
+            scrollTop: target.offset().top
+            }, 500);
+            return false;
+            }
+          }
+      });
+      });
+  });
+
+
+
+
